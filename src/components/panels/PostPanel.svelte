5597a77f6e814a9690d7db239ed025f23520cefd
tomoya8177 <imai@narra.jp>
2023-10-16 11:18:11 +0900

    social icons

--- src/components/panels/PostPanel.svelte
+++ src/components/panels/PostPanel.svelte
@@ -2,14 +2,19 @@
 	import { page } from '$app/stores';
 	import GoBackToPostList from '$components/UIComponents/GoBackToPostList.svelte';
 	import Heading from '$components/UIComponents/Heading.svelte';
+	import Icon from '$components/UIComponents/Icon.svelte';
 	import UserIconNickname from '$components/organisms/UserIconNickname.svelte';
 	import type { Content } from '$lib/frontend/class/Content';
 	import type { Post } from '$lib/frontend/class/Post';
 	import { me } from '$lib/frontend/class/User';
 	import { _ } from '$lib/frontend/i18n';
 	import { locales } from '$lib/frontend/locales';
+	import { onMount } from 'svelte';
 	export let post: Post;
 	export let content: Content;
+	onMount(() => {
+		window.Sharer.init();
+	});
 </script>
 
 <section>
@@ -42,6 +47,38 @@
 		</p>
 	</section>
 	<hr />
+	<section>
+		<div style="display:flex; gap:0.4rem;width:fit-content;margin:auto auto">
+			<button
+				class="secondary"
+				data-sharer="twitter"
+				data-title={content.title}
+				data-hashtags="awesome, sharer.js"
+				data-url={`/post/${post.id}/${content.locale}`}
+				>Share on <i class="fa-brands fa-x-twitter" /></button
+			>
+			<button
+				class="secondary"
+				data-sharer="facebook"
+				data-hashtag="hashtag"
+				data-url={`/post/${post.id}/${content.locale}`}
+				>Share on <i class="fa-brands fa-facebook" /></button
+			>
+			<button
+				class="secondary"
+				data-sharer="line"
+				data-title={content.title}
+				data-url={`/post/${post.id}/${content.locale}`}
+				>Share on <i class="fa-brands fa-line" /></button
+			>
+			<button
+				class="secondary"
+				data-sharer="linkedin"
+				data-url={`/post/${post.id}/${content.locale}`}
+				>Share on <i class="fa-brands fa-linkedin" /></button
+			>
+		</div>
+	</section>
 	<section>
 		<Heading label={_('Read this article in other languages')} />
 		<div style="display:flex; gap:0.4rem;flex-wrap:wrap">
