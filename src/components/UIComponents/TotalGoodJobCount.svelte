7219bfb5133569e822302ba075e62fb90376899d
tomoya8177 <imai@narra.jp>
2023-11-03 11:29:14 +0900

    total good job and filmpgraphy label for search

--- src/components/UIComponents/TotalGoodJobCount.svelte
+++ src/components/UIComponents/TotalGoodJobCount.svelte
@@ -3,6 +3,7 @@
 	import Icon from '$components/UIComponents/Icon.svelte';
 
 	export let count: number;
+	export let withLabel = true;
 </script>
 
 <div style="display:flex;gap:0.2rem">
@@ -10,5 +11,7 @@
 	<span>
 		{count}
 	</span>
-	<small>{_('total Good Jobs')}</small>
+	{#if withLabel}
+		<small>{_('total Good Jobs')}</small>
+	{/if}
 </div>
--- src/components/organisms/EntityCard.svelte
+++ src/components/organisms/EntityCard.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+	import TotalGoodJobCount from '$components/UIComponents/TotalGoodJobCount.svelte';
+	import PopularFilmography from '$components/atoms/PopularFilmography.svelte';
 	import type { Entity } from '$lib/frontend/class/Entity';
+	import { GoodJob } from '$lib/frontend/class/GoodJob';
 	import EntityNameStaticSmall from '../molecules/EntityNameStaticSmall.svelte';
 
 	export let entity: Entity;
 </script>
 
 <article class="card">
-	<EntityNameStaticSmall {entity} />
+	<h3>
+		<EntityNameStaticSmall {entity} />
+	</h3>
+	<PopularFilmography {entity} />
+	<div class="totalGoodJobs">
+		<TotalGoodJobCount withLabel={false} count={entity.totalGoodJobs} />
+	</div>
 </article>
+
+<style>
+	.totalGoodJobs {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
